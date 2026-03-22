2152dba887006ea980b42daa8718b5614aae8da9
bynr-35 <boyner2@gmail.com>
2026-03-22 19:12:53 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.post("/ai", async (req, res) => {
 
   try {
     const response = await fetch(
-      "https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=AIzaSyDbVogn11YWZbWty_nOzLKAS0DULAW1mHM"
+      "https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=AIzaSyBmqLh_sHmgZK4VOXC4aYsxe7sQhlwUwbA"
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
